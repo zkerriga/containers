1c2f03717802879b0179a957287df311d051a6f1
zkerriga <daniil.63@yandex.ru>
2020-12-04 12:37:32 +0300

    Clear list and add ListNode

--- templates/list.hpp
+++ templates/list.hpp
@@ -13,6 +13,7 @@
 #pragma once
 
 #include <memory>
+#include "support/list_node.hpp"
 
 namespace ft {
 
@@ -32,23 +33,15 @@ public:
 	typedef std::size_t						size_type;
 
 	/* Initialize */
-	explicit list( const allocator_type & alloc = allocator_type() )
-		: m_end(_listNode::_createEmptyNode()), m_size(0), m_allocator(alloc)
-	{
-		_listNode::_createEmptyNode(m_allocator);
-		_fillEndNode();
-	}
-	explicit list( size_type n, const value_type & val = value_type(),
-				const allocator_type & alloc = allocator_type() )
-		: m_end(_createEmptyNode()), m_size(n), m_allocator(alloc)
-	{
-		_fillEndNode();
-
-		t_list *	node;
-		for (size_type i = 0; i < n; ++i) {
-			node = _createEmptyNode();
-		}
-	}
+//	explicit list( const allocator_type & alloc = allocator_type() )
+//		: m_end(), m_size(0), m_allocator(alloc)
+//	{
+//	}
+//	explicit list( size_type n, const value_type & val = value_type(),
+//				const allocator_type & alloc = allocator_type() )
+//		: m_end(), m_size(n), m_allocator(alloc)
+//	{
+//	}
 //	template < class InputIterator >
 //	list( InputIterator first, InputIterator last,
 //			const allocator_type & alloc = allocator_type() );
@@ -127,48 +120,12 @@ public:
 //	allocator_type get_allocator() const;
 
 private:
-	struct t_list
-	{
-		value_type *	data;
-		t_list *		next;
-		t_list *		prev;
-	};
-	t_list *		m_end;
+	typedef ListNode<value_type,allocator_type>		_lst;
+
+	_lst *			m_end;
 	size_type		m_size;
 	allocator_type	m_allocator;
 
-	class _listNode {
-	public:
-		value_type *	data;
-		_listNode *			next;
-		_listNode *			prev;
-
-		static _listNode *	_createEmptyNode(allocator_type & alloc) {
-			_listNode *	node = new _listNode;
-
-			node->data = alloc.allocate(1);
-			return node;
-		}
-	}; //class _listNode
-
-	_listNode *		m_End;
-
-	/*t_list *	_createEmptyNode() {
-		t_list *	node = new t_list;
-
-		node->data = m_allocator.allocate(1);
-		return node;
-	}*/
-	void		_fillEndNode() {
-		m_end->prev = m_end;
-		m_end->next = m_end;
-	}
-	t_list *	_fillNode(t_list * targetNode, t_list * nextNode, t_list * prevNode, value_type const & value) {
-		targetNode->data = value;
-		targetNode->next = nextNode;
-		targetNode->prev = prevNode;
-	}
-	t_list *	_nodePushBack(t_list *node) {}
 }; //class list
 
 /* todo */
