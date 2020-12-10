fb0e7390bd88bd4ed66c432a32dbd477ba97ca6c
zkerriga <daniil.63@yandex.ru>
2020-12-10 17:09:24 +0300

    Add types

--- templates/vector.hpp
+++ templates/vector.hpp
@@ -20,6 +20,21 @@ namespace ft {
 
 template < class T, class Alloc = std::allocator<T> >
 class vector {
+public:
+	typedef T								value_type;
+	typedef Alloc							allocator_type;
+	typedef typename Alloc::reference		reference;
+	typedef typename Alloc::const_reference	const_reference;
+	typedef typename Alloc::pointer			pointer;
+	typedef typename Alloc::const_pointer	const_pointer;
+	typedef std::ptrdiff_t					difference_type;
+	typedef std::size_t						size_type;
+
+	/* todo */
+	class iterator {};
+	class const_iterator {};
+	class reverse_iterator {};
+	class const_reverse_iterator {};
 	/* todo */
 };
 
--- unit_tests/vector_tests.cpp
+++ unit_tests/vector_tests.cpp
@@ -23,61 +23,40 @@ using sVec		= std::vector<Any>;
 using mVec		= ft::vector<Any>;
 using sAlloc	= std::allocator<Any>;
 
-/*class ListTestClass: public ::testing::Test {
-public:
-	ListTestClass() {
+class VecTest : public ::testing::Test {
+	VecTest() {
 		// код инициализации
-		for (int i = 0; i < 10; ++i) {
-			sTenList.push_back(i);
-			mTenList.push_back(i);
+		sTen.reserve(10);
+		for (int i = 1; i < 11; ++i) {
+			sTen.push_back(Any(i));
 		}
-		for (int i = 100; i > 0; --i) {
-			sLongList.push_back(i);
-			mLongList.push_back(i);
-		}
+
 		srand(time(nullptr));
-		int size = rand() % 100 + 1;
+		const int size = rand() % 100 + 1;
+		sRandom.reserve(size);
 		int randomInt = 0;
 		for (int i = 0; i < size; ++i) {
 			randomInt = rand();
-			sRandomList.push_front(randomInt);
-			mRandomList.push_front(randomInt);
+			sRandom.push_back(Any(randomInt));
 		}
 	}
-
 	virtual void SetUp() {
 		// код, который будет выполнен перед началом теста
 	}
-
 	virtual void TearDown() {
 		// код, который будет выполнен сразу по завершении теста
 		// при необходимости здесь можно вызывать исключения
 	}
-
-	~ListTestClass()  {
+	~VecTest() {
 		// очистка всех ресурсов, вызов исключений не допускается
-		sEmptyList.clear();
-		sTenList.clear();
-		sLongList.clear();
-		sRandomList.clear();
 	}
-
 public:
-	// сюда можно поместить необходимые вам пользовательские объекты данных
-	sList				sEmptyList;
-	mList				mEmptyList;
+	sVec		sEmpty;
+	sVec		sTen;
+	sVec		sRandom;
 
-	sList				sTenList;
-	mList				mTenList;
-
-	sList				sLongList;
-	mList				mLongList;
-
-	sList				sRandomList;
-	mList				mRandomList;
-
-	sAlloc				sAl;
-};*/
+	sAlloc		sAl;
+};
 
 /*void assertListEQ(const sList & stdList, const mList & myList) {
 	ASSERT_EQ(stdList.size(), myList.size());
@@ -95,7 +74,6 @@ public:
 	}
 	ASSERT_EQ((sIt == sIte), (mIt == mIte));
 }*/
-
 /*void assertListEQFromIterators(sList::const_iterator sIt, sList::const_iterator sIte,
 							   mList::const_iterator mIt, mList::const_iterator mIte) {
 	ASSERT_EQ((sIt == sIte), (mIt == mIte));
@@ -106,7 +84,6 @@ public:
 	}
 	ASSERT_EQ((sIt == sIte), (mIt == mIte));
 }*/
-
 /*void assertListEQFromListAndIterators(sList stdList,
 									  mList::const_iterator mIt, mList::const_iterator mIte) {
 	sList::const_iterator	sIt = stdList.begin();
@@ -120,5 +97,24 @@ public:
 	}
 	ASSERT_EQ((sIt == sIte), (mIt == mIte));
 }*/
+
+TEST(vector, basic_types) {
+	Any	a;
+
+	mVec::value_type				s1 = a;
+	mVec::allocator_type			s2;
+	mVec::reference					s3 = a;
+	mVec::const_reference			s4 = a;
+	mVec::pointer					s5;
+	mVec::const_pointer				s6;
+	mVec::iterator					s7;
+	mVec::const_iterator			s8;
+	mVec::reverse_iterator			s9;
+	mVec::const_reverse_iterator	s10;
+	mVec::difference_type			s11;
+	mVec::size_type					s12;
+}
+
+
 
 #undef DEBUG
